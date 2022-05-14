4f4e230ffd8fc5e53253fce135b63c90df4683f1
Phương HCY <anhthich0k@gmail.com>
2022-05-14 12:50:00 +0700

    add

---
+++ Duolingo_1/Duolingo_1/Duolingo_1/TestPages/BatDauTest.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Duolingo_1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BatDauTest
+    {
+        BaiHoc b;
+        User u;
+        public BatDauTest()
+        {
+            InitializeComponent();
+        }
+        public BatDauTest(BaiHoc bh, User nd)
+        {
+            InitializeComponent();
+            b = bh;
+            u = nd;
+            txtbh.Text = b.TenBH;
+            img.Source = b.Hinh;
+        }
+
+        private void gotoTest_Clicked(object sender, EventArgs e)
+        {
+            if(b.MaBH != 1 && b.MaBH !=2 && b.MaBH !=3)
+            {
+                DisplayAlert("Thông báo", "Câu hỏi chưa được cập nhật. Vui lòng chọn bài học khác.", "OK");
+            }   
+            else
+            {
+                
+                Navigation.PushModalAsync(new TestPage(b, u));
+            
+            }
+            PopupNavigation.Instance.PopAsync();
+
+        }
+    }
+}
